1252c85b44a31fe37802d9daba2f26635f05ee95
Petar Botev <petar.botev@outlook.com>
2025-08-05 16:28:46 +0300

    Added tag helper

---
+++ BethanysPieShop/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SendEmail()
+        {
+            // Logic to send email would go here
+            // For now, just redirect to the index view
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BethanysPieShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string? EmailAddress { get; set; }
+        public string? EmailText { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(EmailAddress))
+            {
+                output.SuppressOutput();
+                return;
+            }
+            var emailLink = $"mailto:{EmailAddress}";
+            var linkText = string.IsNullOrEmpty(EmailText) ? EmailAddress : EmailText;
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", emailLink);
+            output.Content.SetContent(linkText);
+        }
+    }
+}
